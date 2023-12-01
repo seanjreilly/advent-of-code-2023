0b44ad37e8d4fd81105c3564344913edff86291d
Sean Reilly <seanjreilly@gmail.com>
2023-12-01 12:00:34 +0000

    Day 1, Part 1
    Use Char.isDigit, stream operations, and
    concatenation to calculate the answer.

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -5,7 +5,7 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
     val elapsed = measureTimeMillis {
-        val input = readInput("DayXX")
+        val input = readInput("Day01")
         println(part1(input))
         println(part2(input))
     }
@@ -14,9 +14,21 @@ fun main() {
 }
 
 fun part1(input: List<String>): Long {
-    return 0
+    return input.map { firstAndLastDigits(it) }.sumOf { it.toLong() }
 }
 
 fun part2(input: List<String>): Long {
     return 0
+}
+
+internal fun firstAndLastDigits(line: String): String {
+    return firstDigit(line).toString() + lastDigit(line)
+}
+
+internal fun firstDigit(line: String): Char {
+    return line.first { it.isDigit() }
+}
+
+internal fun lastDigit(line: String): Char {
+    return line.last { it.isDigit() }
 }
--- src/test/kotlin/day01/Day01Test.kt
+++ src/test/kotlin/day01/Day01Test.kt
@@ -4,10 +4,38 @@ import org.junit.jupiter.api.Test
 
 class Day01Test {
     private val sampleInput = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
     """.trimIndent().lines()
 
     @Test
-    fun `sample test`() {
-        assert(1 + 2 == 3)
+    fun `firstDigit should return the first digit in the string`() {
+        assert(firstDigit(sampleInput[0]) == '1')
+        assert(firstDigit(sampleInput[1]) == '3')
+        assert(firstDigit(sampleInput[2]) == '1')
+        assert(firstDigit(sampleInput[3]) == '7')
+    }
+
+    @Test
+    fun `lastDigit should return the last digit in the string`() {
+        assert(lastDigit(sampleInput[0]) == '2')
+        assert(lastDigit(sampleInput[1]) == '8')
+        assert(lastDigit(sampleInput[2]) == '5')
+        assert(lastDigit(sampleInput[3]) == '7')
+    }
+
+    @Test
+    fun `firstAndLastDigits should return the first and last digit in the string concatenated together`() {
+        assert(firstAndLastDigits(sampleInput[0]) == "12")
+        assert(firstAndLastDigits(sampleInput[1]) == "38")
+        assert(firstAndLastDigits(sampleInput[2]) == "15")
+        assert(firstAndLastDigits(sampleInput[3]) == "77")
+    }
+
+    @Test
+    fun `part1 should return the sum of the first and last digits of every line`() {
+        assert(part1(sampleInput) == 142L)
     }
 }
