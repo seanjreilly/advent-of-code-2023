b9ff86794eb84abaab466a687b192b86916f1c6d
Sean Reilly <seanjreilly@gmail.com>
2023-12-08 21:22:09 +0000

    Day 8, Part 2
    For part 2,the result is way too big (14 trillion+
    iterations) to calculate directly.
    
    So, start by using the same algorithm as part 1 with a
    different termination condition (ends with Z vs "ZZZ") to
    calculate the individual number of steps for each node
    that starts with A (there are 6 in the prod data). Then
    calculate the cumulative least common multiple of all of
    those counts to get the overall answer.
    
    This method only works when the first leg and subsequent
    loops for each path have the same length, which they do
    for test and production data. Yay. Add assert statements
    to ensure this.
    
    This approach calculates an answer in ~40ms.

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -1,6 +1,7 @@
 package day08
 
 import utils.readInput
+import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -19,19 +20,60 @@ fun part1(input: List<String>): Long {
 
     var steps = 0L
     var key = "AAA"
-    while (key != "ZZZ") {
+    do {
         val currentNode = nodes[key]!!
         val index = (steps++ % directions.length).toInt()
         key = when (directions[index]) {
             'L' -> currentNode.left
             else -> currentNode.right
         }
-    }
+    } while (key != "ZZZ")
     return steps
 }
 
 fun part2(input: List<String>): Long {
-    return 0
+    val nodes = parse(input)
+    val directions = input.first()
+
+    // find all nodes that end with A, calculate the journey length for each node to a node that ends with Z
+    // and then compute the Least Common Multiple of all journey lengths — this is
+    // the number of steps when all the journey periods will synchronize
+    return nodes.keys
+        .filter { it.endsWith("A") }
+        .map { countStepsUntilZNode(nodes, directions, it) }
+        .map { it.toBigInteger() }
+        .reduce(BigInteger::lcm).toLong()
+}
+
+private fun countStepsUntilZNode(nodes: Map<String, LeftRight>, directions: String, startPosition: String) : Long {
+    var key = startPosition
+    var steps = 0L
+    do {
+        val currentNode = nodes[key]!!
+        val index = (steps++ % directions.length).toInt()
+        key = when (directions[index]) {
+            'L' -> currentNode.left
+            else -> currentNode.right
+        }
+    } while (!key.endsWith("Z"))
+
+    val stepsPart1 = steps
+    val terminatorPart1 = key
+
+    do {
+        val currentNode = nodes[key]!!
+        val index = (steps++ % directions.length).toInt()
+        key = when (directions[index]) {
+            'L' -> currentNode.left
+            else -> currentNode.right
+        }
+    } while (!key.endsWith("Z"))
+
+    val stepsPart2 = steps - stepsPart1
+    assert(stepsPart2 == stepsPart1) { "This method only works when the repetition is the same length as the original number of steps" }
+    assert(terminatorPart1 == key) {"This method only works when there's a simple cycle after the first __Z node is reached"}
+//    println("$startPosition -- $stepsPart1 --> $terminatorPart1 -- $stepsPart2 --> $key")
+    return stepsPart1
 }
 
 data class LeftRight(val left: String, val right: String)
@@ -42,4 +84,10 @@ private fun parseLine(line: String): Pair<String, LeftRight> {
     val key = line.substringBefore('=').trim()
     val leftRight = line.substringAfter("(").substringBefore(")").split(", ").let { LeftRight(it[0], it[1]) }
     return Pair(key, leftRight)
+}
+
+internal fun BigInteger.lcm(other: BigInteger): BigInteger {
+    val gcd: BigInteger = this.gcd(other)
+    val absProduct: BigInteger = this.multiply(other).abs()
+    return absProduct.divide(gcd)
 }
--- src/test/kotlin/day08/Day08Test.kt
+++ src/test/kotlin/day08/Day08Test.kt
@@ -22,7 +22,20 @@ class Day08Test {
         BBB = (AAA, ZZZ)
         ZZZ = (ZZZ, ZZZ)
     """.trimIndent().lines()
-    
+
+    private val samplePart2Input = """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+    """.trimIndent().lines()
+
     @Test
     fun `parseNodes should return a map of ids to LeftRight instances given input`() {
         val nodes: Map<String, LeftRight> = parse(sampleInput)
@@ -47,5 +60,10 @@ class Day08Test {
     fun `part1 should count how many steps are needed following the directions to reach ZZZ from AAA`() {
         assert(part1(sampleInput) == 2L)
         assert(part1(sampleInput2) == 6L)
+    }
+    
+    @Test
+    fun `part2 should count how many steps are needed starting from __A puts every journey on a __Z node`() {
+        assert(part2(samplePart2Input) == 6L)
     }
 }
