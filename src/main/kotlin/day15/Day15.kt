3938ab967608c027baaad65569d1b54c6d273264
Sean Reilly <seanjreilly@gmail.com>
2023-12-15 18:47:28 +0000

    Day 15, part 2
    The production data set is 4000 instructions long, so processing each
    instruction using an array list (weirdly, a list of lists is faster
    than an array of lists) of mutable lists calculates the result quickly.
    
    This approach calculates the correct answer for bothparts in ~30ms.

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -14,14 +14,18 @@ fun main() {
 }
 
 fun part1(input: List<String>): Long {
-    return input.first()
-        .split(',')
-        .filter { it.isNotBlank() }
+    return parseInitializationSequence(input)
         .sumOf { it.hash().toLong() }
 }
 
 fun part2(input: List<String>): Long {
-    return 0
+    val boxes = processInitializationSequence(parseInitializationSequence(input))
+
+    return boxes.flatMapIndexed { boxIndex, box ->
+            box.mapIndexed { lensIndex, labelAndFocalLength -> Triple(boxIndex, lensIndex, labelAndFocalLength) }
+        }
+        .map {(boxIndex, lensIndex, labelAndFocalLength) -> (boxIndex + 1) * (lensIndex + 1) * labelAndFocalLength.focalLength}
+        .sumOf { it.toLong() }
 }
 
 internal fun String.hash(): Int {
@@ -33,3 +37,30 @@ internal fun String.hash(): Int {
     }
     return result
 }
+
+private fun parseInitializationSequence(input: List<String>) = input.first().split(',').filter { it.isNotBlank() }
+
+internal fun processInitializationSequence(instructions: List<String>): List<List<LabelAndFocalLength>> {
+    val boxes = List(256) { mutableListOf<LabelAndFocalLength>() }
+    instructions.forEach { instruction ->
+        when {
+            instruction.endsWith('-') -> {
+                val label = instruction.substringBefore('-')
+                val box = boxes[label.hash()]
+                box.removeIf { it.label == label }
+            }
+            else -> {
+                val label = instruction.substringBefore('=')
+                val focalLength = instruction.substringAfter('=').toInt()
+                val box = boxes[label.hash()]
+                when (val index = box.indexOfFirst { it.label == label }) {
+                    -1 -> box.add(LabelAndFocalLength(label, focalLength))
+                    else -> box[index].focalLength = focalLength
+                }
+            }
+        }
+    }
+    return boxes
+}
+
+internal data class LabelAndFocalLength(val label: String, var focalLength: Int)
--- src/test/kotlin/day15/Day15Test.kt
+++ src/test/kotlin/day15/Day15Test.kt
@@ -14,4 +14,29 @@ class Day15Test {
     fun `part1 should read the input line, split on commas, calculate the hash of each segment, and return the sum`() {
         assert(part1(sampleInput) == 1320L)
     }
+
+    @Test
+    fun `part2 should process every intialization step, calculate the focussing power for each lens, and return the sum`() {
+        assert(part2(sampleInput) == 145L)
+    }
+    
+    @Test
+    fun `mutableList_removeAt() should move elements after the index forward by 1`() {
+        val list = mutableListOf(1,2,3,4,5)
+        list.removeAt(list.indexOf(3))
+        assert(list == listOf(1,2,4,5))
+    }
+    
+    @Test
+    fun `processInitializationSequence should follow the initialization instructions and return a list of boxes, with LabelAndFocalLength instances in each box`() {
+        val result:List<List<LabelAndFocalLength>> = processInitializationSequence(sampleInput.first().split(','))
+
+        assert(result.size == 256)
+        assert(result[0] == listOf(LabelAndFocalLength("rn", 1), LabelAndFocalLength("cm", 2)))
+        assert(result[3] == listOf(LabelAndFocalLength("ot", 7), LabelAndFocalLength("ab", 5), LabelAndFocalLength("pc", 6)))
+        val otherBoxes = result.filterIndexed { index, _ -> index !in listOf(0, 3) }
+        assert(otherBoxes.all { it.isEmpty() }  )
+    }
+
+
 }
