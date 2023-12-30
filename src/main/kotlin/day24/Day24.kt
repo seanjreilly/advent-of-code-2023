7649fd7c674ebd220d0fae72d35f5fc78cd6cbf6
Sean Reilly <seanjreilly@gmail.com>
2023-12-30 15:38:54 +0000

    Day 24, Part 1
    Compute intersection using intercept formulas
    hand derived from y = mx+b. M is easy to
    calculate because the deltas for each hailstone are
    provided, so you need to remember "rise over run".
    Once you have M, plug x into y = mx + b to calculate
    b = y - mx.
    
    Once the slope and intercept are calculated for each
    line, the intersection coordinates can be derived
    because we know that x1 = x2 and y1 = y2 at the
    intersection point (because after all, they
    intersect there!). So we can put the two mx + b
    equations equal to each other, replace x2 with x1,
    and rearrange the equation to solve for x1. This
    looks like intersectX = (b2 - b1)/(m1 -m2).
    
    Once we have the x coordinate of the intersection
    point we find the y coordinate with y = mx + b.
    
    Find if the intersection is in the future for each
    point by comparing the sign of (intersectionX - x)
    with the sign of deltaX.
    
    Then test to see if the intersection point is in
    bounds, et voilà.
    
    Compare each possible combination
    of 2 hailstones and count the legal intersections.
    
    (This *sounds* easy, but it was absolute maddening,
    because I wasted an entire day on a *lot* of messing
    around with determinants and other methods of
    calculating intesections that were passing the tests
    but giving wrong answers for production data. Grr.

---
+++ src/main/kotlin/day24/Day24.kt
@@ -0,0 +1,91 @@
+package day24
+
+import utils.LongBounds
+import utils.readInput
+import kotlin.math.sign
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val elapsed = measureTimeMillis {
+        val input = readInput("Day24")
+        println(part1(input))
+        println(part2(input))
+    }
+    println()
+    println("Elapsed time: $elapsed ms.")
+}
+
+fun part1(input: List<String>): Long {
+    val coordinateRange = 200_000_000_000_000L .. 400_000_000_000_000L
+    val bounds = LongBounds(coordinateRange, coordinateRange)
+    return countIntersections(input.map { Hailstone(it) }, bounds)
+}
+
+fun part2(input: List<String>): Long {
+    return 0
+}
+
+internal fun countIntersections(hailstones: List<Hailstone>, bounds: LongBounds): Long {
+    return hailstones
+        .twoElementCombinations()
+        .count { isRelevant2DIntersection(it.first, it.second, bounds) }
+        .toLong()
+}
+
+internal data class Hailstone(val point: LongPoint3D, val velocity: Velocity3D) {
+
+    private val slope = velocity.deltaY.toDouble() / velocity.deltaX
+    private val intercept = point.y - (this.slope * point.x)
+
+    fun findIntersection2D(other: Hailstone): DoublePoint? {
+        //use slope-intercept equations to find an intersection point
+        if (this.slope == other.slope) {
+            return null //lines are parallel
+        }
+        val intersectX = (other.intercept - intercept) / (this.slope - other.slope)
+        val intersectY = (this.slope * intersectX) + this.intercept
+
+        return DoublePoint(intersectX, intersectY)
+    }
+
+    fun isFuture2D(potentialFuturePoint: DoublePoint): Boolean {
+        // this ignores the case where x velocity is zero and a hailstone is moving straight up or down
+        // however, that doesn't occur in my production input
+        if (potentialFuturePoint.x == point.x.toDouble()) { return true } //stones intersect at current position
+        return sign(potentialFuturePoint.x - point.x) == velocity.deltaX.sign.toDouble()
+    }
+
+    companion object {
+        internal operator fun invoke(line: String) : Hailstone {
+            val (x, y, z) = line.substringBefore(" @").split(", ").map { it.trim().toLong() }
+            val (deltaX, deltaY, deltaZ) = line.substringAfter("@ ").split(", ").map { it.trim().toInt() }
+            return Hailstone(LongPoint3D(x,y,z), Velocity3D(deltaX, deltaY, deltaZ))
+        }
+    }
+}
+
+internal data class LongPoint3D(val x: Long, val y: Long, val z: Long)
+internal data class DoublePoint(val x: Double, val y: Double)
+internal data class Velocity3D(val deltaX: Int, val deltaY: Int, val deltaZ: Int)
+
+internal fun isRelevant2DIntersection(hailstoneA: Hailstone, hailstoneB: Hailstone, bounds: LongBounds): Boolean {
+    val intersection = hailstoneA.findIntersection2D(hailstoneB) ?: return false
+    val tests = listOf(hailstoneA::isFuture2D, hailstoneB::isFuture2D, { it in bounds })
+    return tests.all { it.invoke(intersection) }
+}
+
+internal operator fun LongRange.contains(d: Double) = d in (first.toDouble() .. last.toDouble())
+internal operator fun LongBounds.contains(point: DoublePoint) = (point.x in validXCoordinates) && (point.y in validYCoordinates)
+
+internal fun <T> List<T>.twoElementCombinations() : Sequence<Pair<T, T>> {
+    require(size >= 2) { "must have at least 2 elements in the list to generate 2 element combinations" }
+
+    val theList = this
+    return sequence {
+        for (i in 0 until (size - 1)) {
+            for (j in (i + 1) until size) {
+                yield(theList[i] to theList[j])
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/day24/Day24Test.kt
@@ -0,0 +1,136 @@
+package day24
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import utils.LongBounds
+import kotlin.math.truncate
+
+class Day24Test {
+    private val sampleInput = """
+        19, 13, 30 @ -2,  1, -2
+        18, 19, 22 @ -1, -1, -2
+        20, 25, 34 @ -2, -2, -4
+        12, 31, 28 @ -1, -2, -1
+        20, 19, 15 @  1, -5, -3
+    """.trimIndent().lines()
+
+    @Nested
+    inner class HailstoneTest {
+        @Test
+        fun `constructor should extract a 3d point and velocity from an input line`() {
+            val result = Hailstone(sampleInput.first())
+
+            assert(result.point == LongPoint3D(19, 13, 30))
+            assert(result.velocity == Velocity3D(-2, 1, -2))
+        }
+
+        @Test
+        fun `findIntersection2D should return null given another Hailstone that does not intersect with this one`() {
+            val hailstoneA = Hailstone(sampleInput[1])
+            val hailstoneB = Hailstone(sampleInput[2])
+
+            val result = hailstoneA.findIntersection2D(hailstoneB)
+
+            assert(result == null)
+        }
+
+        @Test
+        fun `findIntersection2D should return the intersection point given another Hailstone that intersects with this one`() {
+            val hailstoneA = Hailstone(sampleInput[0])
+            val hailstoneB = Hailstone(sampleInput[1])
+
+            val result = hailstoneA.findIntersection2D(hailstoneB)!!
+
+            assert(truncate3(result.x) == 14.333)
+            assert(truncate3(result.y) == 15.333)
+        }
+
+        @Test
+        fun `isFuture2D returns true given a DoublePoint that the hailstone will encounter in the future`() {
+            val hailstone = Hailstone(sampleInput[0])
+            val potentialFuturePoint = DoublePoint(14.333, 15.333)
+
+            assert(hailstone.isFuture2D(potentialFuturePoint))
+        }
+
+        @Test
+        fun `isFuture2D returns false given a DoublePoint that the hailstone will encounter in the past`() {
+            val hailstoneA = Hailstone(sampleInput[0])
+            val hailstoneB = Hailstone(sampleInput[4])
+
+            val intersection = hailstoneA.findIntersection2D(hailstoneB)!!
+
+            assert(!hailstoneA.isFuture2D(intersection))
+        }
+
+        @Test
+        fun `isFuture2D returns true given a DoublePoint that the hailstone will encounter in the future, even when another hailstone intersects it in the past`() {
+            val hailstoneA = Hailstone(sampleInput[0])
+            val hailstoneB = Hailstone(sampleInput[4])
+
+            val intersection = hailstoneA.findIntersection2D(hailstoneB)!!
+
+            assert(hailstoneB.isFuture2D(intersection))
+        }
+
+        @Test
+        fun `isFuture2D returns true given the DoublePoint that the hailstone is at now`() {
+            val hailstone = Hailstone(sampleInput[0])
+            val potentialFuturePoint = DoublePoint(hailstone.point.x.toDouble(), hailstone.point.y.toDouble())
+
+            assert(hailstone.isFuture2D(potentialFuturePoint))
+        }
+
+    }
+
+    @Test
+    fun `isRelevant2DIntersection should return true iff the hailstones intersect in the future for both hailstones, inside the boundary`() {
+        val hailstones = sampleInput.map { Hailstone(it) }
+        val bounds = LongBounds(7..27, 7..27)
+
+        assert(isRelevant2DIntersection(hailstones[0], hailstones[1], bounds))
+        assert(isRelevant2DIntersection(hailstones[0], hailstones[2], bounds))
+        assert(!isRelevant2DIntersection(hailstones[0], hailstones[3], bounds))
+        assert(!isRelevant2DIntersection(hailstones[0], hailstones[4], bounds))
+
+        assert(!isRelevant2DIntersection(hailstones[1], hailstones[2], bounds))
+        assert(!isRelevant2DIntersection(hailstones[1], hailstones[3], bounds))
+        assert(!isRelevant2DIntersection(hailstones[1], hailstones[4], bounds))
+
+        assert(!isRelevant2DIntersection(hailstones[2], hailstones[3], bounds))
+        assert(!isRelevant2DIntersection(hailstones[2], hailstones[4], bounds))
+
+        assert(!isRelevant2DIntersection(hailstones[3], hailstones[4], bounds))
+    }
+
+    @Test
+    fun `countIntersections should return the number of hailstones that intersect in the future, given a bounds`() {
+        val hailstones = sampleInput.map { Hailstone(it) }
+        val bounds = LongBounds(7..27, 7..27)
+
+        assert(countIntersections(hailstones, bounds) == 2L)
+    }
+
+    @Test
+    fun `twoElementCombinations should return every 2 element combination as a pair`() {
+        val list = listOf("a", "b", "c", "d", "e")
+        val expectedResult = setOf(
+            "a" to "b",
+            "a" to "c",
+            "a" to "d",
+            "a" to "e",
+            "b" to "c",
+            "b" to "d",
+            "b" to "e",
+            "c" to "d",
+            "c" to "e",
+            "d" to "e",
+        )
+
+        assert( list.twoElementCombinations().toSet() == expectedResult)
+    }
+
+    companion object {
+        fun truncate3(double: Double) = truncate(double * 1000) / 1000.0
+    }
+}
