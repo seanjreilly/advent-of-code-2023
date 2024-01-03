55ef11de82653bfd71ce494e391c6bb2e69e7d40
Sean Reilly <seanjreilly@gmail.com>
2024-01-03 22:23:16 +0000

    Day 24, Part 2
    Using factors to find potential velocities had a
    problem, and brute-forcing from -1000..1000 is faster.
    
    A few hundred combinations of x, y, and z velocities
    are proposed, but checking those is quick.
    
    Finding the start point for a propsed velocity
    requires subtracting the hailstone's velocity from
    the proposed velocity — this describes a line
    containing all possible starting positions that can
    hit that hailstone. Finding the intersection of those
    lines (in 3D) finds the rock's start point. Some
    velocities don't yield intersecting lines: these
    aren't potential solutions.
    
    Once the right potential solutions are being proposed,
    the rest of the code finds the right answer. Verify
    potential solutions by testing that they intersect
    with every hailstone (in an integral number of
    nanoseconds). There's actually only one potential
    solution for prod data, but it passes verification.
    
    Production code (especially verification code) is
    longer and less polished than it should be.
    
    This approach calculates an answer in ~70ms.

--- src/main/kotlin/day24/Day24.kt
+++ src/main/kotlin/day24/Day24.kt
@@ -23,7 +23,8 @@ fun part1(input: List<String>): Long {
 }
 
 fun part2(input: List<String>): Long {
-    return 0
+    val (solutionPosition, _) = findRockThatHitsAllHailstones(input.map { Hailstone(it) })
+    return solutionPosition.x + solutionPosition.y + solutionPosition.z
 }
 
 internal fun countIntersections(hailstones: List<Hailstone>, bounds: LongBounds): Long {
@@ -31,6 +32,20 @@ internal fun countIntersections(hailstones: List<Hailstone>, bounds: LongBounds)
         .twoElementCombinations()
         .count { isRelevantXYIntersection(it.first, it.second, bounds) }
         .toLong()
+}
+
+internal fun findRockThatHitsAllHailstones(hailstones: List<Hailstone>): Pair<LongPoint3D, Velocity3D> {
+    // find potential velocities, by taking advantage of hailstones with the same X, Y, and Z velocities
+    val potentialSolutionVelocities = findPotentialRockVelocities(hailstones)
+
+    // turn potential velocities into potential solutions by deriving the initial position (if any) for each velocity
+    val potentialSolutions = potentialSolutionVelocities
+        .mapNotNull { findPotentialRockOrigin(it, hailstones)?.to(it) }
+
+    // we know there is a solution, and we know there's only one
+    // check each potential solution and see if it hits every hailstone
+    return potentialSolutions
+        .first { (position, velocity) -> verifyPotentialPart2Solution(position, velocity, hailstones) }
 }
 
 internal data class Hailstone(val point: LongPoint3D, val velocity: Velocity3D) {
@@ -95,9 +110,7 @@ internal data class Hailstone(val point: LongPoint3D, val velocity: Velocity3D) 
             return null // not actually an intersection
         }
 
-//        check(x == xzIntersection.x.roundToLong()) { "X coordinates from 2 planar intersections don't match. X from XY is ${xyIntersection.x}. X from XZ is ${xzIntersection.x}" }
-
-        val intersection = LongPoint3D(x,y,z)
+        val intersection = LongPoint3D(x, y, z)
         val netVelocity = velocity - other.velocity
         val intersectionTime = (other.point.x - point.x) / netVelocity.deltaX.toLong()
         return intersection to intersectionTime
@@ -142,4 +155,146 @@ internal fun <T> List<T>.twoElementCombinations() : Sequence<Pair<T, T>> {
             }
         }
     }
+}
+
+internal fun verifyPotentialPart2Solution(rockPosition: LongPoint3D, rockVelocity: Velocity3D, hailstones: Collection<Hailstone>): Boolean {
+
+    fun findIntersectionTime(hailstone: Hailstone): Long? {
+        val xClosingVelocity = rockVelocity.deltaX - hailstone.velocity.deltaX
+        return if (xClosingVelocity == 0) {
+            if (rockPosition.x != hailstone.point.x) {
+                return null //rock will never hit the hailstone
+            } else {
+                // parallel and equal in the x-coordinate
+                // try finding t using Y coordinates...
+                val yClosingVelocity = rockVelocity.deltaY - hailstone.velocity.deltaY
+                if (yClosingVelocity == 0) {
+                    if (rockPosition.y != hailstone.point.y) {
+                        return null //rock will never hit the hailstone
+                    } else {
+                        TODO("can't yet handle a case where the rock is parallel to a hailstone in x AND y ")
+                    }
+                }
+                ((hailstone.point.y - rockPosition.y).toBigInteger() / yClosingVelocity.toBigInteger()).toLong()
+            }
+        } else {
+            ((hailstone.point.x - rockPosition.x).toBigInteger() / xClosingVelocity.toBigInteger()).toLong()
+        }
+    }
+
+    fun verify(hailstone: Hailstone) : Boolean {
+        val t = findIntersectionTime(hailstone) ?: return false //no intersection time means no intersection
+        if (t < 0L) {
+            return false //rock and hailstone have to collide in the future (or at time 0?)
+        }
+
+        val rockXAtT = rockPosition.x + (t * rockVelocity.deltaX)
+        val hailstoneXAtT = hailstone.point.x + (t * hailstone.velocity.deltaX)
+
+        if (rockXAtT != hailstoneXAtT) {
+            return false
+        }
+
+        val rockYAtT = rockPosition.y + (t * rockVelocity.deltaY)
+        val hailstoneYAtT = hailstone.point.y + (t * hailstone.velocity.deltaY)
+
+        if (rockYAtT != hailstoneYAtT) {
+            return false
+        }
+
+        val rockZAtT = rockPosition.z + (t * rockVelocity.deltaZ)
+        val hailstoneZAtT = hailstone.point.z + (t * hailstone.velocity.deltaZ)
+
+        return rockZAtT == hailstoneZAtT
+    }
+
+//    val matchCount = hailstones.count { verify(it) }
+//    return matchCount == hailstones.size
+    return hailstones.all { verify(it) }
+}
+
+internal fun findPotentialRockOrigin(potentialRockVelocity: Velocity3D, hailstones: Collection<Hailstone>) : LongPoint3D? {
+    //any two hailstones will do
+    val hailstoneA = hailstones.first()
+    val hailstoneB = hailstones.last()
+
+    // each of these is the line of potential starting points that can hit this hailstone with the given velocity
+    val hailstoneAPrime = hailstoneA.copy(velocity = hailstoneA.velocity - potentialRockVelocity)
+    val hailstoneBPrime = hailstoneB.copy(velocity = hailstoneB.velocity - potentialRockVelocity)
+
+    // the intersection of these two lines is the potential starting point
+    return hailstoneAPrime.findIntersection3D(hailstoneBPrime)?.first
+}
+
+internal fun findPotentialRockVelocities(hailstones: Collection<Hailstone>) : Set<Velocity3D> {
+    val potentialXComponentValues = findPotentialValuesForVelocityComponent(hailstones, { it.velocity.deltaX }, { it.point.x })
+    val potentialYComponentValues = findPotentialValuesForVelocityComponent(hailstones, { it.velocity.deltaY }, { it.point.y })
+    val potentialZComponentValues = findPotentialValuesForVelocityComponent(hailstones, { it.velocity.deltaZ }, { it.point.z })
+
+    return potentialXComponentValues.flatMap { deltaX ->
+        potentialYComponentValues.flatMap { deltaY ->
+            potentialZComponentValues.map { deltaZ ->
+                Velocity3D(deltaX.toInt(), deltaY.toInt(), deltaZ.toInt())
+            }
+        }
+    }.toSet()
+}
+
+internal fun findPotentialValuesForVelocityComponent(
+    hailstones: Collection<Hailstone>,
+    velocityComponentSelector: (Hailstone) -> Int,
+    positionComponentSelector: (Hailstone) -> Long
+): Set<Long> {
+    /*
+    Find the hailstones with duplicate velocity values for this coordinate (i.e., they are parallel in this dimension).
+    Assuming that the rock's position and velocity have to be whole numbers, then a rock that can hit all of these
+    hailstones can have only a few possible velocities in this dimension, because these hailstones are always the
+    same distance apart.
+    */
+
+    val (deltaValue, duplicateHailstones) = hailstones
+        .groupBy(velocityComponentSelector)
+        .entries.maxByOrNull { it.value.size }!!
+
+//        val positiveOrNegativeOne = listOf(1L, -1L)
+//        val plusOrMinusDeltaValue = listOf(deltaValue.toLong(), deltaValue * -1L)
+
+    // find the potential delta values that will work for these 2 hailstones in this velocity coordinate
+    // the rock has to get from p1 to p2 in exactly an integer number of turns
+    // it could be going backwards, and the velocity of the hailstones could be moving towards the rock
+    // or away from it.
+    fun findPotentialDeltaValues(hailstonePair: Pair<Hailstone, Hailstone>): Set<Long> {
+        val (h1, h2) = hailstonePair
+        val p1 = positionComponentSelector(h1)
+        val p2 = positionComponentSelector(h2)
+        val difference = p1 - p2
+
+//            val factors = abs(difference).findFactors() //the rock has to get from p1 to p2 in exactly an integer number of turns
+//            val factorsMethod = factors
+//                .flatMap { factor -> positiveOrNegativeOne.map { it * factor } } // times plus or minus one
+//                .flatMap { value -> plusOrMinusDeltaValue.map { it + value } } // plus or minus this hailstone's velocity component
+//                .toSet()
+//
+//            return factorsMethod
+
+        // using factors to find potential velocities *should* work, but there's a bug in it somewhere :-(
+        // and it's much slower than just brute forcing anyway
+
+        val bruteForceMethod = (-1000 .. 1000)
+            .filter { v -> v != deltaValue }
+            .filter { v -> difference % (v - deltaValue) == 0L }
+            .map { it.toLong() }
+            .toSet()
+
+        return bruteForceMethod
+    }
+
+    //the values have to work for each potential combination of 2 hailstones — throw out anything that doesn't work with a different pair
+    val hailstoneCombinations = duplicateHailstones.twoElementCombinations().toList()
+    val possibleValues = findPotentialDeltaValues(hailstoneCombinations.first()).toMutableSet()
+    hailstoneCombinations
+        .drop(1)
+        .forEach { possibleValues.retainAll(findPotentialDeltaValues(it)) }
+
+    return possibleValues
 }
