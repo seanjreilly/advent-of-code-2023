801a7675fc4b01d59a7f09905ab517c1e6ca2d5d
Sean Reilly <seanjreilly@gmail.com>
2024-11-23 10:42:25 +0000

    Add Puzzle
    The puzzle class determines the day from
    the class name, and automatically downloads
    the appropriate data file from the AoC site
    using stored credentials if necessary (it
    caches downloads in src/main/resources).
    
    Convert the day 1 solution to use Puzzle

---
+++ src/main/kotlin/utils/Puzzle.kt
@@ -0,0 +1,53 @@
+package utils
+
+import java.io.File
+import java.net.URL
+import java.nio.file.Files
+import kotlin.system.measureTimeMillis
+
+abstract class Puzzle<T> {
+    fun run() {
+        print("Advent of Code year ${year}, day ${day}: ")
+        val input = readInput()
+        val elapsed = measureTimeMillis {
+            println(part1(input))
+            println(part2(input))
+        }
+        println()
+        println("Elapsed time: $elapsed ms.")
+    }
+
+    open fun getAoCDay() : Int {
+        val simpleName: String = this::class.simpleName!!
+        return DAY_REGEX.matchEntire(simpleName)!!.groupValues[1].toInt()
+    }
+
+    private val day by lazy { this.getAoCDay() }
+    private val year = "2023"
+
+    private fun readInput() : List<String> {
+        val dataFile = File("src/main/resources/Day${day}.txt")
+        if (dataFile.exists()) {
+            println("using cached download ${dataFile.path}")
+        } else {
+            val url = URL("https://adventofcode.com/${year}/day/${day}/input")
+            println("downloading data file from ${url} to ${dataFile.path}")
+            val credentials = File("src/main/resources/credentials.txt").readText().trim()
+            url.openConnection().apply {
+                setRequestProperty("Cookie", "session=${credentials}")
+                setRequestProperty("Accept", "text/plain")
+            }.getInputStream().use { Files.copy(it, dataFile.toPath()) }
+        }
+        return dataFile.readLines()
+    }
+
+    abstract fun part1(input: List<String>): T
+    abstract fun part2(input: List<String>): T
+
+    companion object {
+        private val DAY_REGEX = """Day(\d{2})""".toRegex()
+    }
+}
+
+abstract class IntPuzzle : Puzzle<Int>()
+abstract class LongPuzzle : Puzzle<Long>()
